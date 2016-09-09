b4d44c454da0cd11c24877abe10bf8935a7b298d
MMarussi <MMarussi@users.noreply.github.com>
2016-09-09 01:17:58 -0300

    Agrego mejoras a proceso entrenador junto con las librerias que paso Kevin para la conexion

--- Entrenador/src/Entrenador.c
+++ Entrenador/src/Entrenador.c
@@ -14,8 +14,12 @@
 #include <commons/config.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
 #include <commons/string.h>
 #include <commons/collections/list.h>
+#include "libSockets.h"
 
 typedef struct {
 	char* nombreEntrenador;
@@ -34,63 +38,23 @@ t_entrenador* crear_Entrenador(char* nombre, int id, int vidas, t_list hoja){
 	return nuevoEntrenador;
 }
 
-char* crearDirectorioEntrenadores(){
+void crearDirectorioDeEntrenador(t_entrenador* entrenador){ //Esto deberia ir en pokedex
+	char* comando_Directorio_Entrenador = string_from_format("mkdir -p /home/utnso/tp-2016-2c-Ni-Lo-Testeamos/PokedexServidor/Entrenadores/%s/", entrenador->nombreEntrenador);
+	system(comando_Directorio_Entrenador); // Crea los directorios Entrenadores (si es que no existe) y el del entrenador en particular
 
-	struct stat st = {0};
-		char* directorio = string_from_format("/home/utnso/tp-2016-2c-Ni-Lo-Testeamos/PokedexServidor/%s", "Entrenadores/");
-		if (stat(directorio, &st) == -1) {
-		    mkdir( directorio, 0700);
-		}
-		return directorio;
+	char* comando_Directorio_Entrenador_Medallas = string_from_format("mkdir -p /home/utnso/tp-2016-2c-Ni-Lo-Testeamos/PokedexServidor/Entrenadores/%s/%s/", entrenador->nombreEntrenador, "medallas");
+	system(comando_Directorio_Entrenador_Medallas); // Crea el directorio medalla en la ruta del entrenador particular
+
+	char* comando_Directorio_Entrenador_Metadata = string_from_format("mkdir -p /home/utnso/tp-2016-2c-Ni-Lo-Testeamos/PokedexServidor/Entrenadores/%s/%s/", entrenador->nombreEntrenador, "metadata");
+	system(comando_Directorio_Entrenador_Metadata); // Crea el directorio metadata en la ruta del entrenador particular
+
+	char* comando_Directorio_Entrenador_DirBill = string_from_format("mkdir -p /home/utnso/tp-2016-2c-Ni-Lo-Testeamos/PokedexServidor/Entrenadores/%s/%s/", entrenador->nombreEntrenador, "Dir\\ de\\ Bill");
+	system(comando_Directorio_Entrenador_DirBill);
 }
-
-int crearDirectorioDeEntrenador(t_entrenador* entrenador, char* directorio){ //Esto deberia ir en pokedex
-
-	struct stat st = {0};
-	char* subdirectorio = string_from_format(directorio, entrenador->nombreEntrenador);
-	if (stat(subdirectorio, &st) == -1) {
-	    mkdir( subdirectorio, 0700);
-	}
-	// crearSubdirectorios(directorio); No hace lo esperado(no hace nada en realidad)
-	return 0;
-}
-
-/*int crearSubdirectorios(char* directorio){ // Revisar, no funciona y se podria mejorar (sacar appends)
-	struct stat st = {0};
-		char* medalla = string_new();
-		string_append(&medalla,directorio);
-		string_append(&medalla,"/medalla");
-		if (stat(directorio, &st) == -1) {
-		    mkdir( medalla, 0700);
-		}
-
-		char* dirBill = string_new();
-		string_append(&dirBill,directorio);
-		string_append(&dirBill,"/Dir de Bill/");
-		if (stat(directorio, &st) == -1) {
-		    mkdir( dirBill, 0700);
-		}
-		//string_from_format(directorio,"/metadata");
-		char* subdirectorio = string_from_format(directorio, "/metadata");
-		//system(command);
-		if (stat(subdirectorio, &st) == -1) {
-			    mkdir( subdirectorio, 0700);
-			}
-		char* metadata = string_new();
-		string_append(&metadata,directorio);
-		string_append(&metadata,"/metadata");
-		if (stat(directorio, &st) == -1) {
-		    mkdir( metadata, 0700);
-		}
-
-		return 0;
-}
-*/
 
 int main(void) {
 	t_list hojaPrueba;
 	t_entrenador* prueba = crear_Entrenador("Prueba",1,3,hojaPrueba);
-	char* directorioEntrenadores = crearDirectorioEntrenadores();
-	crearDirectorioDeEntrenador(prueba, directorioEntrenadores);
+	crearDirectorioDeEntrenador(prueba);
 		return 0;
 	}
---
+++ Entrenador/src/libSockets.h
@@ -0,0 +1,89 @@
+/*
+ * libSockets.h
+ *
+ *  Created on: 1/9/2016
+ *      Author: utnso
+ */
+
+#ifndef LIBSOCKETS_H_
+#define LIBSOCKETS_H_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <commons/log.h>
+#include <commons/string.h>
+
+typedef struct Paquete {
+	int codigoOperacion;
+	int programCounter;
+	int pid;
+	int quantum;
+	int tamanio;
+	char *path;
+} Paquete;
+
+/* setup_listen(IP,PORT) *
+ * Devuelve el socket que se consiguió para escuchar
+ *
+ * IP = Ip de escucha
+ * PORT = Puerto de escucha
+ */
+int setup_listen(char*, char*);
+
+/* setup_listen(IP,PORT,LOGGER) *
+ * Variante del setup_listen con la posibilidad de loggeo
+ *
+ * IP = Ip de escucha
+ * PORT = Puerto de escucha
+ * LOGGER = Log para escribir
+ */
+int setup_listen_con_log(char*, char*, t_log *);
+
+/* cargarInfoSocket(IP,PORT) *
+ * Devuelve un addr configurado
+ *
+ * IP = Ip de escucha
+ * PORT = Puerto de escucha
+ */
+struct addrinfo* cargarInfoSocket(char *, char*);
+
+/* conectarCliente(IP,PORT) *
+ * Devuelve el socket que se conectó con el servidor
+ *
+ * IP = Ip de escucha
+ * PORT = Puerto de escucha
+ */
+int conectarCliente(char *IP, char* Port);
+
+/* conectarCliente_con_log(IP,PORT,LOGGER) *
+ * Devuelve el socket que se conectó con el servidor
+ *
+ * IP = Ip de escucha
+ * PORT = Puerto de escucha
+ * LOGGER = Log para escribir
+ */
+int conectarCliente_con_log(char *IP, char* Port, t_log *);
+
+/* esperarConexionEntrante(SocketEscucha,Backlog,LOGGER)
+ * Devuelve el socket para mandar datos
+ *
+ * LOGGER = Log para escribir
+ */
+int esperarConexionEntrante(int, int, t_log *);
+
+int conectarServidor(char* IP, char* Port, int backlog);
+Paquete *generarPaquete(int codigoOperacion, int tamMessage, char *message,
+		int programCounter, int quantum, int pid);
+char *serializar(Paquete *unPaquete);
+Paquete *deserializar_header(char *buffer);
+void deserializar_data(Paquete *unPaquete, char *buffer);
+void destruirPaquete(Paquete * unPaquete);
+
+
+#endif /* LIBSOCKETS_H_ */
