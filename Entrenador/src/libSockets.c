607445c0236777a6e2d60f0a272ca7f4349f023c
GuidoOliveri <guido.oli@hotmail.com>
2016-09-12 15:11:38 -0300

    Config de entrenador arreglado, ahora si funciona lo de config get array

--- Entrenador/src/libSockets.c
+++ Entrenador/src/libSockets.c
@@ -14,7 +14,7 @@ int leerConfigEnt(char *ruta, t_entrenador **datos){
 		return 0;
 	} else {
 		int cantidadKeys = config_keys_amount(archivoConfiguracion);
-			if (cantidadKeys < 6) {
+			if (cantidadKeys < 8) {
 				return 0;
 			} else {
 			char* nombre=string_new();
@@ -23,22 +23,17 @@ int leerConfigEnt(char *ruta, t_entrenador **datos){
 			(*datos)->caracter=config_get_string_value(archivoConfiguracion,"simbolo");
 			(*datos)->hojaDeViaje=config_get_array_value(archivoConfiguracion,"hojaDeViaje");
 
-			// ahora que obtuve las cosas lo manejo como lista ambas
-			t_list* listaDeViaje;
-			t_list* listaObjetivosxMapa;
 			int i=1;
+			int j =0;
 			do{
-				int j =0;
 				if ((*datos)->hojaDeViaje[j]!=NULL){
-					// ARREGLAR ESTO MAS TARDE
 					char* objetivoDeMapa = string_from_format("obj[%s]",(*datos)->hojaDeViaje[j]);
-					list_add(listaDeViaje,(*datos)->hojaDeViaje[j]);
+					printf("tengo que cumplir %s \n", objetivoDeMapa);
 					(*datos)->objetivosPorMapa= config_get_array_value(archivoConfiguracion,objetivoDeMapa);
-					list_add(listaObjetivosxMapa, (*datos)->objetivosPorMapa[j]);
 					j++;
 				}
 				else {
-					i =-1;
+					i=0;
 				}
 			} while(i);
 
