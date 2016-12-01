fcea57fbcf4d9ac6c3cbba893e8061c71ebf4fa3
GuidoOliveri <guido.oli@hotmail.com>
2016-11-30 23:21:56 -0300

    Faltaban estos, ahora si estan todos

---
+++ Entrenador/src/scriptMisty.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd
+cd nilotesteamos/tp-2016-2c-Ni-Lo-Testeamos/Entrenador/src
+
+gcc Entrenador.c libSockets.c libSockets.h -o entrenador -lcommons -lfuse -lpthread
+
+./entrenador Misty /home/utnso/nilotesteamos/mnt
+
